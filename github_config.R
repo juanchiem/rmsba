93d3b7beb764cc079eb2e9e831a07e06352d6128
AnibalCerrudo <anibal.cerrudo@gmail.com>
2021-09-30 15:44:24 -0300

    subo script 2021

---
+++ github_config.R
@@ -0,0 +1,6 @@
+library(usethis)
+use_git_config(user.name = "AnibalCerrudo", 
+               user.email = "anibal.cerrudo@gmail.com")
+usethis::create_github_token() 
+credentials::set_github_pat("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")
+
